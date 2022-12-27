8eff0303ab07d9da4b1062df8cfd24dba2423878
babar <babersdn@gmail.com>
2022-12-27 22:14:59 +0500

    FrontEnd and backend connected with POST Fetch AP

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import {Form ,Button,Tabs, Tab, Col, Row, Container } from "react-bootstrap";
-
+import { Form, Button, Tabs, Tab, Col, Row, Container } from "react-bootstrap";
 
 export function Login() {
-  const [key, setKey] = useState()
+  const [key, setKey] = useState();
 
   return (
     <Tabs
@@ -22,41 +21,58 @@ export function Login() {
   );
 }
 
-;
-
 export function LoginTab() {
-//   const [apiData, setApiData] = useState([]);
-//   useEffect(() => {
-// const fetchData= async()=> {
-//       const data = await fetch("http://localhost:3000/api");
-//       const realData = await data.json();
-//       console.log(realData);
-//       setApiData(realData);
-//     } 
-    
-//   fetchData()
+  const [loginPhone, setLoginPhone] = useState("");
+  const [password, setPassword] = useState("");
   
-  
-    
-//   },[])
-  
+  async function handleSubmit() {
+    await fetch("http://localhost:3000/api",{
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        phone : loginPhone,
+        password: password,
+    }),
+  }
+  )
+  console.log(loginPhone)
+  console.log(password+"handle submit works")
+}
   return (
     <Container className=" login-cs w-75 rounded-4 mt-3  p-5 ">
-      
-       
-      
-      <Form method="post" action="http://localhost:5173/"  className="w-75 mx-auto">
+      <Form onSubmit={handleSubmit} className="w-75 mx-auto">
         <Form.Group className="mb-3" controlId="loginPhone">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control name="phone" type="number" placeholder="Enter Phone Number" />
-          <Form.Text className="text-muted"  >
-            You have to enter your number to Log-In 
+          <Form.Control
+            value={loginPhone}
+            onChange={(e) => {
+              setLoginPhone(e.target.value);
+            }}
+            name="phone"
+            type="number"
+            placeholder="Enter Phone Number"
+          />
+          <Form.Text className="text-muted">
+            You have to enter your number to Log-In
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="pass" type="password" placeholder="Password" />
+          <Form.Control
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            name="pass"
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -72,41 +88,39 @@ export function LoginTab() {
 export function SignUp() {
   return (
     <Container className=" login-cs w-75 rounded-4 mt-3  p-5 ">
-    <Form className=" mx-auto">
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="SignupFName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter First Name" />
+      <Form className=" mx-auto">
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="SignupFName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter First Name" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="SignupLName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Last Name" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="SignupNumber">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="number" placeholder="Enter Phone Number" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="SignupLName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Last Name" />
+        <Form.Group className="mb-3" controlId="SignupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-      </Row>
 
-      <Form.Group className="mb-3" controlId="SignupNumber">
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="number" placeholder="Enter Phone Number" />
-      </Form.Group>
+        <Form.Group className="mb-3" id="formGridCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="SignupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Row className="mb-3">
-      
-      <Button  variant="primary" type="submit">
-        Sign Up
-      </Button>
-      </Row>
-    </Form>
+        <Row className="mb-3">
+          <Button variant="primary" type="submit">
+            Sign Up
+          </Button>
+        </Row>
+      </Form>
     </Container>
   );
 }
--- server/app.js
+++ server/app.js
@@ -10,74 +10,21 @@ const DATABASE_URL = "mongodb://localhost:27017";
 DBConnect(DATABASE_URL);
 
 app.use(express.json());
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-  console.log("root path getted");
+ });
+app.post("/api", (req, res) => {
+  console.log(req.body);
 });
-app.get("/login", (req,res)=>{
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.send("login");
-  console.log(req.body);
-  console.log("app get worls");
-})
-app.post("/login", (req,res)=>{
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.send("login");
-  console.log(req.body);
-  console.log(req.body.loginPhone);
-  console.log("app post short url");
-})
-app.get("http://localhost:5173/login", (req,res)=>{
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.send("login");
-  console.log(req.body);
-  console.log(req.body.loginPhone);
-  console.log("app post full url");
-})
-
-
-// app.get("login", (req, res) => {
-//   const phone = req.body
-//   res.send("hello login fro server")
-//   console.log(phone)
-//   console.log("hey")
-// });
-// app.post("/login", (req, res) => {
- 
-//    const phone = req.body
-//   res.send("hello login fro server")
-//   console.log(phone)
-//   console.log("h")
-// });
-// app.get("/api", (req, res) => {
-//   res.send({
-//     name: "Babar",
-//     email: "sample@server.com",
-//     accEmail: "babatnetf2@gmail.com",
-//     password: "babardsi",
-//     userNo:"05",
-//     userPin:"7897",
-//     validTill: "11-12-2002",
-//     paymentStatus:"paid",
-    
-//   });
-// });
-
 app.listen(port, () => {
   console.log(`Server is running on localhost:${port}`);
 });
