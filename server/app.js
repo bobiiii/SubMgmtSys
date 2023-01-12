32185d4739b6590ed29387a72c2c4f113010e8fa
babar <babersdn@gmail.com>
2023-01-12 20:19:56 +0500

    added routes and controllers

--- server/app.js
+++ server/app.js
@@ -3,12 +3,13 @@ import DBConnect from "./mongodb/DBConnect.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import {DB} from "./dummyDB/DB.js"
+import login_routes from "./routes/login.js";
+
 
 const app = express();
 const port = 3000;
-const DATABASE_URL = "mongodb://localhost:27017";
-
-DBConnect(DATABASE_URL);
+// const DATABASE_URL = "mongodb://localhost:27017";
+// DBConnect(DATABASE_URL);
 
 app.use(express.json());
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -20,31 +21,45 @@ app.use(
 );
 app.use(cors());
 
+app.use("/login", login_routes)
+
 app.get("/", (req, res) => {
   res.send("res from server");
  });
+
+app.post("/signup",(req,res)=>{
+  console.log("signup")
+  })
+
+app.listen(port, () => {
+  console.log(`Server is running on localhost:${port}`);
+});
+
+
+
+
 
 //  let arr =[{
 //   phone: "12345",
 //   password: "54321",
 
 // }]
-app.get("/login",(req,res)=>{
-  res.send(arr)
-})
+// app.get("/login",(req,res)=>{
+//   res.send(arr)
+// })
 
-app.post("/login", (req, res) => {
-  const phone = req.body.phone
-  const password = req.body.password
-  const phoneExist= DB.find(db => db.phone === phone && db.password === password)
-  if (phoneExist) {
-    res.send("Success, Registered User")
-  } else {
+// app.post("/login", (req, res) => {
+//   const phone = req.body.phone
+//   const password = req.body.password
+//   const phoneExist= DB.find(db => db.phone === phone && db.password === password)
+//   if (phoneExist) {
+//     res.send("Success, Registered User")
+//   } else {
     
-    res.status(400).send(" Server::REjected, Non-Reg User")
-    console.log(DB)
+//     res.status(400).send(" Server::REjected, Non-Reg User")
+//     console.log(DB)
     
-  }
+//   }
   
   
   // let phn =phone.parseInt()
@@ -64,14 +79,4 @@ app.post("/login", (req, res) => {
   //   res.send("asd")
     
   // }
-});
-
-
-app.post("/signup",(req,res)=>{
-  console.log("signup")
-  
-  }
-)
-app.listen(port, () => {
-  console.log(`Server is running on localhost:${port}`);
-});
+// });
